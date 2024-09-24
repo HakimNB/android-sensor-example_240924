21cdb235eab12ef61b514c7794fc014793564873
Hakim Hauston <hakimh@google.com>
2024-09-24 15:14:14 +0800

    [LS] sensor: hook up onSensorChanged

--- app/src/main/java/com/forgameloft/sensorchangecallback/MainActivity.java
+++ app/src/main/java/com/forgameloft/sensorchangecallback/MainActivity.java
@@ -1,12 +1,41 @@
 package com.forgameloft.sensorchangecallback;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
 import android.view.View;
 
 import com.google.androidgamesdk.GameActivity;
 
-public class MainActivity extends GameActivity {
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends GameActivity implements SensorEventListener {
     static {
         System.loadLibrary("sensorchangecallback");
+    }
+
+    private SensorManager mSensorManager;
+    private Sensor mHingeSensor;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mHingeSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HINGE_ANGLE);
+        List<Sensor> allSensor = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        Log.d("MainActivity", "MainActivity.onStart");
+        for(final Sensor s: allSensor) {
+            Log.d("MainActivity", "MainActivity.sensor: " + s.getName());
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mHingeSensor, SensorManager.SENSOR_DELAY_GAME);
     }
 
     @Override
@@ -28,5 +57,38 @@ public class MainActivity extends GameActivity {
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
         );
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Log.d("MainActivity", "MainActivity.onSensorChanged: " + sensorEvent.toString() +
+                " timestamp: " + sensorEvent.timestamp +
+                " accuracy: " + sensorEvent.accuracy +
+                " firstEventAfterDiscontinuity: " + sensorEvent.firstEventAfterDiscontinuity +
+                " sensor: " + sensorEvent.sensor.getName() +
+                " values: " + Arrays.toString(sensorEvent.values)
+        );
+
+        // Device
+        // 2024-09-24 14:45:46.959 29183-29183 MainActivity            com...gameloft.sensorchangecallback  D  MainActivity.onAccuracyChanged: {Sensor name="Hinge Angle (wake-up)", vendor="Google", version=1, type=36, maxRange=180.0, resolution=5.0, power=0.001, minDelay=0} i = 3
+        // 2024-09-24 14:45:46.959 29183-29183 MainActivity            com...gameloft.sensorchangecallback  D  MainActivity.onSensorChanged: android.hardware.SensorEvent@8e088d2 timestamp: 351529014864209 accuracy: 3 firstEventAfterDiscontinuity: false sensor: Hinge Angle (wake-up) values: [0.0]
+        // 2024-09-24 14:45:47.723 29183-29183 MainActivity            com...gameloft.sensorchangecallback  D  MainActivity.onSensorChanged: android.hardware.SensorEvent@5d834ae timestamp: 352851581212937 accuracy: 3 firstEventAfterDiscontinuity: false sensor: Hinge Angle (wake-up) values: [5.0]
+        // 2024-09-24 14:45:47.724 29183-29183 MainActivity            com...gameloft.sensorchangecallback  D  MainActivity.onSensorChanged: android.hardware.SensorEvent@8e088d2 timestamp: 352851581212937 accuracy: 3 firstEventAfterDiscontinuity: false sensor: Hinge Angle (wake-up) values: [5.0]
+        // 2024-09-24 14:45:47.886 29183-29183 MainActivity            com...gameloft.sensorchangecallback  D  MainActivity.onSensorChanged: android.hardware.SensorEvent@5d834ae timestamp: 352851777423147 accuracy: 3 firstEventAfterDiscontinuity: false sensor: Hinge Angle (wake-up) values: [50.0]
+        // 2024-09-24 14:45:47.887 29183-29183 MainActivity            com...gameloft.sensorchangecallback  D  MainActivity.onSensorChanged: android.hardware.SensorEvent@8e088d2 timestamp: 352851777423147 accuracy: 3 firstEventAfterDiscontinuity: false sensor: Hinge Angle (wake-up) values: [50.0]
+        // 2024-09-24 14:45:47.894 29183-29183 MainActivity            com...gameloft.sensorchangecallback  D  MainActivity.onSensorChanged: android.hardware.SensorEvent@5d834ae timestamp: 352851797304474 accuracy: 3 firstEventAfterDiscontinuity: false sensor: Hinge Angle (wake-up) values: [55.0]
+        // 2024-09-24 14:45:47.895 29183-29183 MainActivity            com...gameloft.sensorchangecallback  D  MainActivity.onSensorChanged: android.hardware.SensorEvent@8e088d2 timestamp: 352851797304474 accuracy: 3 firstEventAfterDiscontinuity: false sensor: Hinge Angle (wake-up) values: [55.0]
+        // 2024-09-24 14:45:47.924 29183-29183 MainActivity            com...gameloft.sensorchangecallback  D  MainActivity.onSensorChanged: android.hardware.SensorEvent@5d834ae timestamp: 352851817185802 accuracy: 3 firstEventAfterDiscontinuity: false sensor: Hinge Angle (wake-up) values: [60.0]
+        // 2024-09-24 14:45:47.924 29183-29183 MainActivity            com...gameloft.sensorchangecallback  D  MainActivity.onSensorChanged: android.hardware.SensorEvent@8e088d2 timestamp: 352851817185802 accuracy: 3 firstEventAfterDiscontinuity: false sensor: Hinge Angle (wake-up) values: [60.0]
+        // 2024-09-24 14:45:47.932 29183-29183 MainActivity            com...gameloft.sensorchangecallback  D  MainActivity.onSensorChanged: android.hardware.SensorEvent@5d834ae timestamp: 352851837066804 accuracy: 3 firstEventAfterDiscontinuity: false sensor: Hinge Angle (wake-up) values: [65.0]
+        // 2024-09-24 14:45:47.932 29183-29183 MainActivity            com...gameloft.sensorchangecallback  D  MainActivity.onSensorChanged: android.hardware.SensorEvent@8e088d2 timestamp: 352851837066804 accuracy: 3 firstEventAfterDiscontinuity: false sensor: Hinge Angle (wake-up) values: [65.0]
+        // 2024-09-24 14:45:48.963 29183-29183 MainActivity            com...gameloft.sensorchangecallback  D  MainActivity.onSensorChanged: android.hardware.SensorEvent@5d834ae timestamp: 352852870885230 accuracy: 3 firstEventAfterDiscontinuity: false sensor: Hinge Angle (wake-up) values: [60.0]
+        // 2024-09-24 14:45:48.964 29183-29183 MainActivity            com...gameloft.sensorchangecallback  D  MainActivity.onSensorChanged: android.hardware.SensorEvent@8e088d2 timestamp: 352852870885230 accuracy: 3 firstEventAfterDiscontinuity: false sensor: Hinge Angle (wake-up) values: [60.0]
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        Log.d("MainActivity", "MainActivity.onAccuracyChanged: " + sensor.toString() + " i = " + i);
     }
 }
